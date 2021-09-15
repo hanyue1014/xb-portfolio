62a92a5f83addba662339e313c334505bd25c7c7
Sky Hanyue <skyhanyue@pop-os.localdomain>
2021-09-15 21:50:15 +0800

    Add header component file, didn't done anything yet

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
-  <h1>Hello World</h1>
+  <Header />
 </template>
 
 <script>
+import Header from './components/Header.vue'
 
 export default {
   name: 'App',
   components: {
+    Header
   }
 }
 </script>
