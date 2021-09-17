4eb22c7fde4e916524649a141511884814de3117
hanyue1014 <wongxuanbin@gmail.com>
2021-09-17 21:28:18 +0800

    Basic 'Profile Picture' component + styling

---
+++ src/components/Profile.vue
@@ -0,0 +1,20 @@
+<template>
+  <img src="../assets/logo.png" alt="Pfp">
+</template>
+
+<script>
+export default {
+  name: 'Profile'
+}
+</script>
+
+<style scoped>
+/* MAKE IT ROUND */
+img {
+  width: 32px;
+  height: 32px;
+  border-radius: 50%;
+  border: 2px solid black;
+  margin: 4px 8px 0px 0px;
+}
+</style>
